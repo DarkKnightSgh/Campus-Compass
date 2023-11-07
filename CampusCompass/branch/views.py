1f433494e0bfea6288ce33042464e22aa2f8c2a2
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-11-07 21:28:46 +0530

    US003 F001 feature #3 add branch models and build API

--- CampusCompass/branch/views.py
+++ CampusCompass/branch/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Branch
+from django.http import JsonResponse
+
 # Create your views here.
+
+def get_branches(request, department_id):
+    branches = {}
+    if (Branch.objects.filter(department_id=department_id).exists()):
+        branches = Branch.objects.filter(department_id=department_id).values('branch_code', 'branch_name')
+    return JsonResponse({'branches': list(branches)})
