a21c0a28a00de4fcfb1f6c76534241f429f5d93d
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-11-08 00:36:13 +0530

    US006 #6 all tasks done(unit testing not needed as inbuilt django functions used)

--- CampusCompass/account/urls.py
+++ CampusCompass/account/urls.py
@@ -23,5 +23,36 @@ urlpatterns= [
     # mentor
     path('mentor_registration',views.mentor_registration,name='mentor_registration'),
 
+    # forgot / change password
+    # Change Password
+    path(
+        'change-password/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='account/change-password.html',
+            success_url = '/account/login'
+            ) ,  name='change_password'),
+    # Forget Password
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='account/password-reset.html',
+             subject_template_name='account/password_reset_subject.txt',
+             email_template_name='account/password_reset_email.html',
+            #  success_url='login'
+         ),
+         name='password_reset'),
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='account/password-reset-done.html'), 
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='account/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='account/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+
+
 
 ]
