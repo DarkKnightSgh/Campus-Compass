5857c1a333a06cefac3cadae9b1a468b1e867b32
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-11-07 23:11:27 +0530

    US011 #11 all tasks completed (unit testing done)

--- CampusCompass/branch/admin.py
+++ CampusCompass/branch/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+from .models import Branch,Department
+
 # Register your models here.
+
+class BranchAdmin(admin.ModelAdmin):
+    list_display=(
+        'department',
+        'branch_name',
+    )
+    search_fields=[
+        'department',
+        'branch_name',
+        
+    ]
+    list_editable= ['branch_name']
+    list_filter=['department']
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display=[
+        'department_name'
+    ]
+    
+    search_fields=[
+        'department_name',
+        
+    ]
+
+admin.site.register(Branch,BranchAdmin)
+
+admin.site.register(Department,DepartmentAdmin)
+
