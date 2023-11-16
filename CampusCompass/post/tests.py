f0314ed8c9d8461ccb7184cd0f4fbc6ae9f108e4
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-11-17 00:07:08 +0530

    US016 #16 all unit tests done

--- CampusCompass/post/tests.py
+++ CampusCompass/post/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Post, PostComment, Branch,Department
+from .forms import PostForm
 
 class PostModelTest(TestCase):
     def setUp(self):
@@ -45,3 +47,50 @@ class PostCommentModelTest(TestCase):
     def test_comment_creation(self):
         self.assertEqual(self.comment.comment, 'This is a test comment.')
         self.assertEqual(str(self.comment.user), 'testuser')
+
+
+class PostFormTest(TestCase):
+    def setUp(self):
+        self.department = Department.objects.create(department_name="test_dept")
+        self.branch = Branch.objects.create(branch_name='Test Branch',department=self.department)
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def test_post_form(self):
+        form_data = {
+            'title': 'Test Post',
+            'content': 'This is a test post.',
+            'tags': 'test, post',
+            'branch': [self.branch],
+        }
+        form = PostForm(data=form_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.cleaned_data['title'], 'Test Post')
+        self.assertEqual(form.cleaned_data['content'], 'This is a test post.')
+        self.assertEqual(list(form.cleaned_data['branch']), [self.branch])
+
+
+class MakePostViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.department = Department.objects.create(department_name="test_dept")
+        self.branch = Branch.objects.create(branch_name='Test Branch',department=self.department)
+        self.url = reverse('make_post')  # replace 'make_post' with the actual name of the view
+
+    def test_make_post_view_GET(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('form' in response.context)
+
+    def test_make_post_view_POST(self):
+        self.client.login(username='testuser', password='12345')
+        form_data = {
+            'title': 'Test Post',
+            'content': 'This is a test post.',
+            'tags': 'test, post',
+            'branch': [self.branch],
+        }
+        response = self.client.post(self.url, data=form_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('form' in response.context)
