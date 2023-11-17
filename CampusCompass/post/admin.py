9634db07840877cead1a2e7368cbf2264ffea476
shreyamishra414@gmail.com <shreyamishra414@gmail.com>
2023-11-17 21:35:19 +0530

    Update post.admin

--- CampusCompass/post/admin.py
+++ CampusCompass/post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+from .models import PostComment
+
+admin.site.register(Post)
+admin.site.register(PostComment)
