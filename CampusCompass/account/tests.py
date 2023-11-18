89207afe32629a7e989e2010fd8c3325af959cb3
Siri Gowri H <sirigowriharish@outlook.com>
2023-11-18 14:26:06 +0530

    US007 #7 Mentor

--- CampusCompass/account/tests.py
+++ CampusCompass/account/tests.py
@@ -2,15 +2,13 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from branch.models import Department, Branch
 from .models import Student, Mentor, Club, ClubMember
-from taggit.managers import TaggableManager
 from taggit.models import Tag
-
 
 class ModelTestCase(TestCase):
     def setUp(self):
         # Create test objects for dependencies
         self.department = Department.objects.create(department_name='Test Department')
-        self.branch = Branch.objects.create(branch_name='Test Branch',department= self.department)
+        self.branch = Branch.objects.create(branch_name='Test Branch', department=self.department)
 
         # Create a test user
         self.user = User.objects.create_user(
@@ -19,8 +17,8 @@ class ModelTestCase(TestCase):
             email='test@example.com'
         )
 
-    def test_student_model(self):
-        student = Student.objects.create(
+        # Create a test student
+        self.student = Student.objects.create(
             student_id='SRN123',
             bio='Test Bio',
             user=self.user,
@@ -34,45 +32,23 @@ class ModelTestCase(TestCase):
             show_number=True,
             year_of_passing_out=2023
         )
-
-        self.assertEqual(student.__str__(), 'testuser')  # Assuming __str__ returns username
 
     def test_mentor_model(self):
-        student = Student.objects.create(
-            student_id='SRN123',
-            bio='Test Bio',
-            user=self.user,
-            department=self.department,
-            branch=self.branch,
-            college_email='test@example.com',
-            whatsapp_number='1234567890',
-            whatsapp_link='http://example.com',
-            email_confirmed=True,
-            is_mentor=False,
-            show_number=True,
-            year_of_passing_out=2023
-        )
-
         mentor = Mentor.objects.create(
-            student=student,
+            student=self.student,
+            username='mentor_user',
             resume='path/to/resume.pdf',
             description='Test Mentor Description',
             approved=True
         )
-        domain_name="aiml"
+        domain_name = "aiml"
         domain, created = Tag.objects.get_or_create(name=domain_name)
         mentor.domain.add(domain)
+        self.assertTrue(mentor.approved)
 
-    def test_club_model(self):
-        club = Club.objects.create(
-            club_name='Test Club',
-            club_desc='Test Club Description',
-            club_logo="/path/to/logo.jpg"
-        )
-
-        club.branch.add(self.branch)
-
-        self.assertEqual(club.__str__(), 'Test Club')
+        # Add mentorship-related assertions here
+        # For example, you can check if the mentor is associated with the correct student
+        self.assertEqual(mentor.student, self.student)
 
     def test_club_member_model(self):
         club = Club.objects.create(
@@ -88,4 +64,16 @@ class ModelTestCase(TestCase):
             social_media_manager=False
         )
 
+        # Adjust the expected result based on your __str__ implementation
         self.assertEqual(club_member.__str__(), 'testuser (Test Club)')
+
+    def test_club_model(self):
+        club = Club.objects.create(
+            club_name='Test Club',
+            club_desc='Test Club Description',
+            club_logo="/path/to/logo.jpg"
+        )
+
+        club.branch.add(self.branch)
+
+        self.assertEqual(club.__str__(), 'Test Club')
